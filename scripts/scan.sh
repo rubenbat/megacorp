12d0e23693cd8350f3cd52157b251f35ae3e7ac0
Ruben Batista <ruben.batista@live.com>
2024-12-25 09:43:52 +0000

    K: sensitive info scanning
    K: credit card scanning
    
    L: ssn numbers scanning
    
    M: phone numbers scanning

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,12 @@
-# TODO: write the script
+echo "\n ==== SCANNIG FOR CREDIT CARD NUMBERS ====="
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git}
+echo "\n ==== CREDIT CARD SCAN COMPLETE ====="
+
+echo "\n ==== SCANNIG FOR SS NUMBERS ====="
+grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git}
+echo "\n ==== SOCIAL SECURITY NUMBERS SCAN COMPLETE ====="
+
+echo "\n ==== SCANNIG FOR PHONE NUMBERS ====="
+grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git}
+grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git}
+echo "\n ==== PHONE NUMBERS SCAN COMPLETE ====="
